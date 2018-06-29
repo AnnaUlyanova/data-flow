d9ae8d3f5a2f589b79a2ec89ff2958efabaa0c76
AnnaUlyanova <a.ulyanova89@gmail.com>
2018-06-30 00:00:22 +1200

    add year selector

--- client/src/components/ByMonths.jsx
+++ client/src/components/ByMonths.jsx
@@ -2,18 +2,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-function ByMonths ({ monthlyExpenses }) {
-  if (!monthlyExpenses) {
+function ByMonths ({ years, allYearsData, showDataForYear, monthsForSelectedYear,  expensesForSelectedYear}) {
+  if (!years || !allYearsData) {
     return (
-      <div>
-        <h1>Null</h1>
-      </div>
+      <p>No data available</p>
     )
+  }
+
+  function handleOnChange(event) {
+    console.log('event', event.target.value)
+    const yearString = 'year '.concat(event.target.value)
+    return showDataForYear(allYearsData, yearString)
   }
   return (
     <div>
       <h1>By Months</h1>
-      <p>{monthlyExpenses}</p>
+      <select onChange={handleOnChange}>
+        {years.map((year) =>
+          <option value={year}>{year}</option>
+        )}
+      </select>
+      {monthsForSelectedYear && <p>dataForSelectedYear: {monthsForSelectedYear}, {expensesForSelectedYear}</p>}
     </div>
   )
 }
--- client/src/containers/By-Months.js
+++ client/src/containers/By-Months.js
@@ -1,26 +1,50 @@
 import {
    compose,
    lifecycle,
+   withStateHandlers,
  } from 'recompose'
 
  import {
-   keys
+   keys,
+   map,
+   replace,
+   propOr,
+   values
  } from 'ramda'
 
  import { annualExpeneseByMonths } from '../main-data'
  import ByMonths from '../components/ByMonths'
 
+ const getYears = annualExpeneseByMonths.then((data) => map(replace(/year /, ''), keys(data)))
+
  export const enhance = compose(
-   lifecycle({
-     componentDidMount(){
-         annualExpeneseByMonths.then((monthlyExpenses) => {
-           const years = keys(monthlyExpenses)
-              this.setState({
-                monthlyExpenses: years
-              })
-         })
+  lifecycle({
+    componentDidMount(){
+      getYears.then((years) => {
+        this.setState({
+          years: years
+        })
+      })
+      annualExpeneseByMonths.then((allYearsData) => {
+        this.setState({
+          allYearsData: allYearsData
+        })
+      })
      }
-   })
+  }),
+  withStateHandlers(
+    ({ initialData = [] }) => ({
+      dataForSelectedYear: initialData
+    }),
+    {
+      showDataForYear: ({ dataForSelectedYear }) => (allYearsData, year) => {
+        return ({
+          monthsForSelectedYear: keys(propOr('', year, allYearsData)[0]),
+          expensesForSelectedYear: values(propOr('', year, allYearsData)[0])
+        })
+      }
+   }
  )
+)
 
  export default enhance(ByMonths)
